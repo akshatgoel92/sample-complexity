48b0021da3bd720470d60210b8e970ff9ac0abc0
akshatgoel92 <akshatgoel92@hotmail.com>
2020-12-07 02:36:02 +0530

    Add test to kernel perceptron and add first pass at boosting

---
+++ src/multi_class_adaboost.py
@@ -0,0 +1,44 @@
+# Import packages
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+def boost_init(m):
+    '''
+    Initialize boosting weights
+    '''
+    D = np.array([1/m]**m)
+    return(D)
+
+
+def boost_choose_alpha(D, preds, labels):
+    '''
+    Initialize boosting weights
+    '''
+        error = np.dot(D, np.sum(preds != labels)/len(labels))
+        alpha = 0.5*np.log((1 - error/error))
+    return(alpha)
+
+
+def boost_update(D, alpha, labels, preds):
+    '''
+    Update the distribution over the training examples
+    '''
+    D = np.dot(D, np.exp(-1*np.dot(alpha, np.dot(labels, preds.T))))
+    Z = np.sum(D)
+    D = D/Z
+    return(D)
+
+
+def boost_train():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ '__main__':
+    main()
