232fecb7abbb802fd43b30bda7f3c195ef97072b
akshatgoel92 <akshatgoel92@hotmail.com>
2020-12-27 00:25:15 +0530

    Add next pass at winnow

--- complexity/winnow.py
+++ complexity/winnow.py
@@ -1,63 +1,10 @@
-import numpy as np
-
-'''
-Input: (x1,y1),...,(xm,ym)∈{0,1}n ×{0,1} ⃗
-1. Initialise w1 = 1.
-2. For t=1 to m do
-3. Receive pattern: xt ∈ {0, 1}^n 
-4. Predict: 
-    0 if wt·xt<n
-yˆ = t
-5. Receive label: yt ∈ {0, 1} 
-6. If mistake (yˆ ̸= y )
-• Update: wt+1,i = wt,i 2(yt −yˆt ) xt,i (1 ≤ i ≤ n). 
-7. End For
-'''
-
-def winnow(set, weights, theta, learningRate=1):
-
-    # Initialize error count
-    errors = 0
-    
-    # Initiate the feature vector
-    wt = array([0] * len(weights))
-    
-    # For each entry t in the training data set
-    for xt, yt in set:
-        
-        # Convert label to the winnow specification
-        yt = -1 if yt == 0 else 1
-        
-        # Set the respective values to the feature vector
-        wt *= 0
-        for word in xt:
-            wt[dictionary[word]] = 1
-        
-        # Calculate the dot product
-        wx = dot(weights, wt)
-        
-        # If error then update weight vector
-        if wx < theta and yt == 1:
-            errors += 1
-            weights += 2.0 * wt
-        if wx > theta and yt == -1:
-            errors += 1
-            weights += 0.5 * wt
-            
-    # Returns the results
-    return [weights, errors]
-
-
-
-
-
 # Import packages
 import helpers
 import numpy as np
 
 
 
-def train_perceptron(X, Y, X_val, Y_val, epochs, lr):
+def train_winnow(X, Y, X_val, Y_val, epochs, lr):
     '''
     --------------------
     Perceptron algorithm
@@ -98,28 +45,29 @@ def train_perceptron(X, Y, X_val, Y_val, epochs, lr):
             x_i = X[i, :]
             y_i = Y[i]
 
+            y_i = 0 if yt == -1
+
             
             # Compute the prediction with the current weights
-            if (np.dot(w, x_i) > 0): y_hat = 1
-            else: y_hat = -1
+            if (np.dot(w, x_i) + b < n): y_hat = 0
+            else: y_hat = 1
             
             # Check if the prediction is correct against the labels
             # If it is correct we don't need to make any updates: we just move to the next iteration
             # If it is not correct then we do the following: 
             # 1) Update the weights and biases in the direction of the label
             if y_hat != y_i:
-                mistakes+=1
-                print(mistakes)
-                w += (y_i - y_hat)*x_i
-                b += (y_i - y_hat)
+                w *= np.power(2, (y_i - y_hat)*x_i)
+                b *= np.power(2, (y_i - y_hat))
                 
             
             
         # Get predictions on train and test
-        y_train_preds = np.sign(np.dot(X, w) + b)
-        y_train_preds[y_train_preds==0] = -1
+        y_train_preds = (np.dot(X_train, w) + b) >= n
+        y_val_preds = (np.dot(X_train, w) + b) >= n
         
-        y_val_preds = np.sign(np.dot(X_val, w) + b)
+        # Change encoding back
+        y_train_preds[y_train_preds==0] = -1
         y_val_preds[y_val_preds==0] = -1
 
         # Training accuracy
@@ -140,7 +88,7 @@ def train_perceptron(X, Y, X_val, Y_val, epochs, lr):
     return(history)
 
 
-def get_perceptron_baseline(m, n, epochs, lr):
+def get_winnow(m, n, epochs, lr):
     '''
     --------------------
     Run perceptron algorithm to get a base-line
@@ -165,7 +113,7 @@ def get_perceptron_baseline(m, n, epochs, lr):
     X_train, X_val, Y_train, Y_val = helpers.split_data(X, Y, 0.8)
     
     # Call the perceptron training with the given epochs
-    history = train_perceptron(X_train, Y_train, X_val, Y_val, epochs, lr)
+    history = train(X_train, Y_train, X_val, Y_val, epochs, lr)
     
     # Return statement
     return(history)
