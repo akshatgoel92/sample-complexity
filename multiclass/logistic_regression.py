c19ed461817754a3983854d73d4ab54d228d622a
akshatgoel92 <akshatgoel92@hotmail.com>
2020-12-31 16:51:45 +0530

    Add modified logistic regression function as a class

--- multiclass/logistic_regression.py
+++ multiclass/logistic_regression.py
@@ -3,124 +3,120 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def get_initial_weights(n_classes, n_features):
-    '''
-    Initialize the weight and bias vectors
-    '''
-    W = np.zeros((n_classes, n_features + 1))
+
+class LogisticRegression():
+
+
+    def __init__(self, lr, epochs, n_classes, n_features, X_train, Y_train):
+
+        
+        self.lr = lr 
+        self.history = []
+        
+        self.epochs = epochs
+        self.n_classes = n_classes
+        self.X_train = X_train
+        
+        self.Y_train = Y_train.astype(int)
+        self.n_examples = len(self.Y_train)
+        
+        self.n_features = min(self.X_train.shape)
+        self.W = np.zeros((self.n_classes, self.n_features + 1))
+        self.Y_encoding = self.get_one_hot_encoding(self.Y_train, self.n_classes)
+
+        
     
-    return(W)
+    def get_one_hot_encoding(self, Y_train, n_classes):
+        '''
+        Get one hot encoded labels for 1 vs. all
+        '''
+        Y = np.full(Y_train.size*n_classes, 0).reshape(Y_train.size, n_classes)
+        Y[np.arange(Y_train.size), Y_train] = 1
+
+        return(Y)
 
 
-def get_one_hot_encoding(Y_train, n_classes):
-    '''
-    --------------------------------------
-    Get one hot encoded labels for 1 vs. all
-    '''
-    Y = np.full(Y_train.size*n_classes, 0).reshape(Y_train.size, n_classes)
-    Y[np.arange(Y_train.size), Y_train] = 1
-
-    return(Y)
-
-
-def add_bias(X):
-    '''
-    Takes in n_examples * n_features
-    Returns (n_examples * n_features + 1) matrix
-    '''
-    return(np.hstack((np.ones((max(X.shape),1)),X)))
+    def add_bias(self, X):
+        '''
+        Takes in n_examples * n_features
+        Returns (n_examples * n_features + 1) matrix
+        '''
+        return(np.hstack((np.ones((max(X.shape),1)),X)))
 
 
 
-def get_cost(Y_encoding, a, n_examples):
-    '''
-    Returns cross entropy loss
-    '''
-
-    return -np.sum(Y_encoding * np.log(a)) / n_examples
-
+    def get_cost(self, Y_encoding, a, n_examples):
+        '''
+        Returns cross entropy loss
+        '''
+        return -np.sum(Y_encoding * np.log(a)) / n_examples
 
 
-def get_softmax(a):
-    '''
-    Takes in a vector of probabilities and 
-    outputs the thresholded predictions
-    '''
-    return np.exp(a) / np.sum(np.exp(a), axis=1).reshape(a.shape[0], 1)
+
+    def get_softmax(self, a):
+        '''
+        Takes in a vector of probabilities and 
+        outputs the thresholded predictions
+        '''
+        return np.exp(a) / np.sum(np.exp(a), axis=1).reshape(a.shape[0], 1)
 
 
-def predict_train(X, W):
-    '''
-    Generate the prediction
-    '''
-    a = get_softmax(np.dot(X, W.T))
+    def predict_softmax(self, X, W):
+        '''
+        Generate the prediction
+        '''
+        a = self.get_softmax(np.dot(X, W.T))
+        
+        return(a)
 
-    return(a)
+
+    def get_gradient_descent_step(self, a):
+        '''
+        Update weights according to gradient descent rule
+        '''
+        delta = (self.lr/self.n_examples) * np.dot((a-self.Y_encoding).T, self.X_train)
+        self.W -= delta
 
 
-def get_gradient_descent_step(lr, a, X, Y, n_examples, W):
-    '''
-    Update weights according to gradient descent rule
-    '''
-    delta = (lr/n_examples) * np.dot((a-Y).T, X)
-    W -= delta
+    def train(self):
+        '''
+        Update weights according to gradient descent rule
+        '''
+
+        self.X_train = self.add_bias(X_train)
+
+        for epoch in range(self.epochs):
+
+            a = self.predict_softmax(self.X_train, self.W)
+            cost = self.get_cost(self.Y_encoding, a, self.n_examples)
+            print(cost)
+
+            W = self.get_gradient_descent_step(a) 
+            self.history.append(cost)
+
+        return(self.history, self.W)
+
+        
+
+
+    def predict(self, X, Y):
+        '''
+        Predict on validation set
+        '''
+        X = self.add_bias(X)
+        Y = Y.astype(int)
+
+        n_val_examples = len(Y)
+        preds = self.predict_softmax(X, self.W)
+        preds = np.argmax(preds, axis = 1)
+        loss = helpers.get_loss(Y, preds)
+
+        return(preds, loss)
+
+
+def test():
+
     
-    return(W)
-
-
-def train(X_train, Y_train, lr = 0.01, n_classes = 10, epochs = 100):
-    '''
-    Update weights according to gradient descent rule
-    '''
-    history = []
-    
-    n_features = min(X_train.shape)
-
-    n_examples = len(Y_train)
-
-    X_train = add_bias(X_train)
-
-    Y_train = Y_train.astype(int)    
-
-    Y_encoding = get_one_hot_encoding(Y_train, n_classes)
-    
-    W = get_initial_weights(n_classes, n_features)
-
-    for epoch in range(epochs):
-
-        a = predict_train(X_train, W)
-
-        cost = get_cost(Y_encoding, a, n_examples)
-
-        W = get_gradient_descent_step(lr, a, X_train, Y_encoding, n_examples, W) 
-
-        history.append(cost)
-
-    return(W, history)
-
-
-def predict(X_val, Y_val, W):
-    '''
-    Predict on validation set
-    '''
-
-    X_val = add_bias(X_val)
-
-    Y_val = Y_val.astype(int)
-
-    n_val_examples = len(Y_val)
-
-    preds_val = predict_train(X_val, W)
-
-    preds_val = np.argmax(preds_val, axis = 1)
-
-    val_loss = helpers.get_loss(Y_val, preds_val)
-
-    return(val_loss)
-
-
-
-if __name__ == '__main__':
 
     np.random.seed(13290)
 
@@ -142,9 +138,25 @@ if __name__ == '__main__':
     # Split dataset
     X_train, X_val, Y_train, Y_val, _, _ = helpers.split_data(X_shuffle, Y_shuffle, perm, data_args['train_percent'])
 
+    # Create weak learner object
+    weak_learner = LogisticRegression(lr=0.01, epochs=10, n_classes=10, 
+                                      n_features=256, X_train=X_train, Y_train=Y_train)
+
     # Train
-    W, history = train(X_train, Y_train)
+    weak_learner.train()
+
+    # Get training predictions and loss
+    train_preds, train_loss = weak_learner.predict(X_train, Y_train)
 
     # Evaluate
-    val_loss = predict(X_val, Y_val, W)
+    val_preds, val_loss = weak_learner.predict(X_val, Y_val)
 
+    return(train_preds, train_loss, val_preds, val_loss)
+
+
+
+if __name__ == '__main__':
+
+    train_preds, train_loss, val_preds, val_loss = test()
+
+    print(train_preds, train_loss, val_preds, val_loss)
