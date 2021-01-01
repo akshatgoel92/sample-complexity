6278196740df6029cd1ef2f2930b159851504427
akshatgoel92 <akshatgoel92@hotmail.com>
2021-01-01 10:39:02 +0530

    Add k nearest neighbors

--- multiclass/knn.py
+++ multiclass/knn.py
@@ -19,11 +19,13 @@ class KNN:
         
         # Initialize empty matrix to store predictions
         # Store no. of validation samples
-        self.predictions = []
+        self.train_predictions = []
+        self.val_predictions = []
+        self.n_train_samples = len(self.Y_train)
         self.n_val_samples = len(self.Y_val)
 
 
-    def get_euclidean_dist(self, X, Y):
+    def get_euclidean_dist(self, X, Y, fill=False):
         '''
         --------------------------
         Load data from source
@@ -45,46 +47,99 @@ class KNN:
         # 1) Element wise product and then sum horizontally
         # 2) Dot product between A and B.T
         dist = np.sqrt(np.einsum('ij,ij->i',X, X)[:,None] + np.einsum('ij,ij->i',Y,Y) - 2*np.dot(X,Y.T))
+
+        if fill == True:
+            np.fill_diagonal(dist, 0)
+
         return(dist)
 
 
-
     def train(self):
+
+        
+        train_distance = self.get_euclidean_dist(self.X_train, self.X_train, fill=True)
+
+        train_sorted_distances = np.argsort(train_distance, axis = 1)[:, :self.k + 1]
+
+        # Loop through train samples
+        for j in range(self.n_train_samples):
+        
+            # Labels of nearest neighbors
+            sorted_distance = train_sorted_distances[j]
+
+            sorted_distance = sorted_distance[sorted_distance != j]
+
+            print(sorted_distance)
+
+            labels = self.Y_train[sorted_distance]
+
+            print(len(labels))
+        
+            # Predictions based on nearest neighbor labels
+            candidates, votes = np.unique(labels, return_counts = True)
+            
+            # Print candidates and votes as a check during testing
+            print("Votes:")
+            print(candidates, votes)
+
+            # Make a decision by majority vote
+            decision = np.where(votes == np.max(votes))[0].astype(int)
+            candidates = candidates[decision]
+
+            # Print the final decision as a check by majority vote
+            print("Decision:")
+            print(decision)
+            print(candidates)
+
+            # Break ties if there is more than one maximum vote
+            if len(votes > 1):
+                self.train_predictions.append(np.random.choice(candidates, size=1)[0])
+            # Otherwise just append the single final decision to the predictions vector 
+            else:
+                self.train_predictions.append(candidates[0])
+
+
+
+    def validate(self):
         '''
         Train one nearest neighbor
         '''
         # Calculate distance between validation point and each training point
-        distance = self.get_euclidean_dist(self.X_val, self.X_train)
+        val_distance = self.get_euclidean_dist(self.X_val, self.X_train)
         
-        sorted_distances = np.argsort(distance, axis = 1)[:, :self.k]
+        val_sorted_distances = np.argsort(val_distance, axis = 1)[:, :self.k]
 
         # Loop through validation samples
         for i in range(self.n_val_samples):
         
             # Labels of nearest neighbors
-            self.labels = self.Y_train[sorted_distances[i]]
+            labels = Y_train[val_sorted_distances[i]]
         
             # Predictions based on nearest neighbor labels
-            self.candidates, self.votes = np.unique(self.labels, return_counts = True)
+            candidates, votes = np.unique(labels, return_counts = True)
+            print("Votes:")
+            print(candidates, votes)
 
-            print(self.candidates, self.votes)
+            decision = np.where(votes == np.max(votes))[0].astype(int)
 
-            decision = np.where(self.votes == np.max(self.votes))[0].astype(int)
+            print("Decision:")
+            print(decision)
 
-            self.candidates = self.candidates[decision]
+            candidates = candidates[decision]
 
-            
-            if len(self.votes > 1):
+            print(candidates)
 
-                self.predictions.append(np.random.choice(self.candidates, size=1)[0])
+            if len(votes > 1):
+                self.val_predictions.append(np.random.choice(candidates, size=1)[0])
 
             else:
-                self.predictions.append(self.candidates[0])
+                self.val_predictions.append(candidates[0])
 
         # Calculate validation loss
-        val_loss = helpers.get_loss(self.Y_val, self.predictions)
+        train_loss = helpers.get_loss(self.Y_train, self.train_predictions)
+        val_loss = helpers.get_loss(self.Y_val, self.val_predictions)
 
-        return(val_loss)
+        return(train_loss, val_loss)
 
 
 if __name__ == '__main__':
@@ -106,7 +161,7 @@ if __name__ == '__main__':
     # Shuffle and split dataset
     X_shuffle, Y_shuffle, perm = helpers.shuffle_data(X,Y)
 
-    k = 20
+    k = 9
     
     # Split dataset
     X_train, X_val, Y_train, Y_val, _, _ = helpers.split_data(X_shuffle, Y_shuffle, perm, data_args['train_percent'])
@@ -114,6 +169,7 @@ if __name__ == '__main__':
     # Call the perceptron training with the given epochs
     knn = KNN(k, X_train, Y_train, X_val, Y_val)
 
-    val_loss = knn.train()
+    train_loss = knn.train()
+    val_loss = knn.validate()
 
-    print(val_loss)
+    print(train_loss, val_loss)
